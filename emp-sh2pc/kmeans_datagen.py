32f146763078d1bc318e89412ae07b4847a6b988
Lele Chen <lchen63@ur.rochester.edu>
2019-12-05 20:45:06 -0500

    Add files via upload

--- emp-sh2pc/kmeans_datagen.py
+++ emp-sh2pc/kmeans_datagen.py
@@ -1,6 +1,9 @@
 import random
+import sklearn
 a = 10
 b = 2
+# fx = open("./1.dat", 'w')
+# fy = open("./2.dat", 'w')
 
 fx = open("data/kmeans/1.dat", 'w')
 fy = open("data/kmeans/2.dat", 'w')
@@ -10,28 +13,35 @@ ys = []
 init = str(a) + ',' + str(b)
 fx.write("%s\n"%init)
 fy.write("%s\n"%init)
+tmp = []
 for i in range(a):
 	tmpx = []
 	tmpy = []
 	str1 = ''
 	str2 = ''
-
+	gg1 = []
+	gg2 = []
 	for j in range(b):
-
 		if j != 0:
 			str1 += ','
 			str2 += ','
-		# tmpx.append(random.uniform(0, 100))
-		# tmpy.append(random.uniform(0, 100))
-		str1 += str(random.uniform(0, 100))
+		# tmp.append(random.uniform(0, 100))
+		aa = random.uniform(0, 100)
+		bb = random.uniform(0, 100)
+		gg1.append(aa)
+		gg2.append(bb)
+		# tmp.append(random.uniform(0, 100))
+		str1 += str(aa)
 
-		str2 += str(random.uniform(0, 100))
-
+		str2 += str(bb)
+	tmp.append(gg1)
+	tmp.append(gg2)
 	fx.write("%s\n"%str1)
 	fy.write("%s\n"%str2)
-# 	xs.append(tmpx)
-# 	ys.append(tmpy)
-# print (xs)
-# print(ys)
+print (tmp)
+c, l = sklearn.cluster.k_means(tmp, n_clusters= 3,n_init = 10)
+print (c,l)
+
+
 
 
